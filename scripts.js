3fb48401d541a90b8146b6d45a6e8ec9376ee76d
lprice93 <lprice@wilbron.com>
2019-03-20 23:10:25 -0500

    DOM drill 1 - initial push

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let body = document.createElement('header-container');
+
+    let h1 = document.createElement('h1');
+    let h1Text = document.createTextNode("This is an h1!");
+    h1.appendChild(h1Text);
+    document.body.appendChild(h1);
+    
+    )}
